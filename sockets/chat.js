aa59920853e0cc79cecaf15e3fdb3eb7777bafc7
Allen Stevens <allen.stevens@students.dominican.edu>
2025-10-01 20:14:00 -0700

    Route messages to correct channel

--- sockets/chat.js
+++ sockets/chat.js
@@ -1,5 +1,6 @@
 //chat.js
-module.exports = (io, socket, onlineUsers) => {
+//Make sure to add channels to module.exports parameters
+module.exports = (io, socket, onlineUsers, channels) => {
   socket.on('new user', (username) => {
     //Save the username as key to access the user's socket id
     onlineUsers[username] = socket.id;
@@ -14,16 +15,26 @@ module.exports = (io, socket, onlineUsers) => {
     socket.emit('get online users', onlineUsers);
   })
 
-  //Listen for new messages
+  //Listen for new messages with channels
   socket.on('new message', (data) => {
-    // Send that data back to ALL clients
-    console.log(`🎤 ${data.sender}: ${data.message} 🎤`)
-    io.emit('new message', data);
+    //Save the new message to the channel.
+    channels[data.channel].push({sender : data.sender, message : data.message});
+    //Emit only to sockets that are in that channel room.
+    io.to(data.channel).emit('new message', data);
   })
 
-  // Register initial new channel event (logging only)
   socket.on('new channel', (newChannel) => {
-    console.log(newChannel);
+    //Save the new channel to our channels object. The array will hold the messages.
+    channels[newChannel] = [];
+    //Have the socket join the new channel room.
+    socket.join(newChannel);
+    //Inform all clients of the new channel.
+    io.emit('new channel', newChannel);
+    //Emit to the client that made the new channel, to change their channel to the one they made.
+    socket.emit('user changed channel', {
+      channel : newChannel,
+      messages : channels[newChannel]
+    });
   });
 
   // This fires when a user closes out of the application
